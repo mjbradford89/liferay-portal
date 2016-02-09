38d42e02268517e1936c8f10455bf9e76dc9a68b
michael.bradford@liferay.com <michael.bradford@liferay.com>
2016-02-09 15:09:32 -0800

    LPS-62973 - Permissions configuration opens in a new page after changing display style

--- modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/icon.js
+++ modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/icon.js
@@ -5,28 +5,54 @@ AUI.add(
 			register: function(config) {
 				var instance = this;
 
-				var icon = A.one('#' + config.id);
+				var id = '#' + config.id;
 
 				var forcePost = config.forcePost;
 				var src = config.src;
 				var srcHover = config.srcHover;
 				var useDialog = config.useDialog;
 
-				if (icon) {
-					if (srcHover) {
-						instance._onMouseOver = A.rbind('_onMouseHover', instance, srcHover);
-						instance._onMouseOut = A.rbind('_onMouseHover', instance, src);
+				var delegateNode = instance._findSuitableDelegateNode(id);
 
-						icon.hover(instance._onMouseOver, instance._onMouseOut);
-					}
+				if (srcHover) {
+					instance._onMouseOver = A.rbind('_onMouseHover', instance, srcHover);
+					instance._onMouseOut = A.rbind('_onMouseHover', instance, src);
 
-					if (useDialog) {
-						icon.on('click', instance._useDialog, instance);
-					}
-					else if (forcePost) {
-						icon.on('click', instance._forcePost, instance);
-					}
+					instance._eventHandles.push(delegateNode.delegate('hover', instance._onMouseOver, instance._onMouseOut, id));
 				}
+
+				if (useDialog) {
+					instance._eventHandles.push(delegateNode.delegate('click', instance._useDialog, id));
+				}
+				else if (forcePost) {
+					instance._eventHandles.push(delegateNode.delegate('click', instance._forcePost, id));
+				}
+			},
+
+			_findSuitableDelegateNode: function(id) {
+				var instance = this;
+
+				var childNode = A.one(id);
+				var delegateNode = null;
+
+				if (childNode) {
+					delegateNode = childNode.ancestor(
+						function(parentNode) {
+							if (parentNode.hasClass('dropdown') ||
+								parentNode.hasClass('list-group-item') ||
+								parentNode.hasClass('portlet-body')) {
+
+								return true;
+							}
+						}
+					);
+				}
+
+				if (delegateNode == null) {
+					delegateNode = A.getDoc();
+				}
+
+				return delegateNode;
 			},
 
 			_forcePost: function(event) {
@@ -49,6 +75,27 @@ AUI.add(
 				}
 			},
 
+			_onSurfaceEndNavigate: function(id) {
+				var instance = this;
+
+				var remainingEventHandles = A.Array.filter(
+					instance._eventHandles,
+					function(handle) {
+						var id = handle.sub.filter;
+
+						if (A.one(id) == null) {
+							handle.detach();
+
+							return false;
+						}
+
+						return true;
+					}
+				);
+
+				instance._eventHandles = remainingEventHandles;
+			},
+
 			_useDialog: function(event) {
 				Liferay.Util.openInDialog(
 					event,
@@ -58,10 +105,16 @@ AUI.add(
 						}
 					}
 				);
-			}
+			},
+
+			_eventHandles: []
 		};
 
 		Liferay.Icon = Icon;
+
+		if (Liferay.PropsValues.SINGLE_PAGE_APPLICATION_ENABLED) {
+			Liferay.on('surfaceEndNavigate', Liferay.Icon._onSurfaceEndNavigate, Liferay.Icon);
+		}
 	},
 	'',
 	{
