6e3ff3ce13597368ec1c17b6ec066a53189482de
michael.bradford@liferay.com <michael.bradford@liferay.com>
2016-08-09 10:06:05 -0700

    friendly url mapper

---
+++ modules/apps/foundation/angular-portlet/portlet/src/main/java/portlet/portlet/route/AngularPortletFriendlyURLMapper.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.calendar.web.internal.portlet.route;
+
+import com.liferay.portal.kernel.portlet.DefaultFriendlyURLMapper;
+import com.liferay.portal.kernel.portlet.FriendlyURLMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.friendly-url-routes=META-INF/friendly-url-routes/routes.xml",
+		"javax.portlet.name=AngularPortlet"
+	},
+	service = FriendlyURLMapper.class
+)
+public class AngularPortletFriendlyURLMapper extends DefaultFriendlyURLMapper {
+}
